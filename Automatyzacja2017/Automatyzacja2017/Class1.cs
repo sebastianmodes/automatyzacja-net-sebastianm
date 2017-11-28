294baf0bebfd2e3f8d04e9746f632f822e318124
Sebastian Modes <ibaspl@gmail.com>
2017-11-28 11:25:30 +0100

    Dodanie XUnit oraz stworzenie metod Automatyzacja2017

--- Automatyzacja2017/Automatyzacja2017/Class1.cs
+++ Automatyzacja2017/Automatyzacja2017/Class1.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Automatyzacja2017
 {
-    public class Class1
+    public class Matematyka
     {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+
+        public double Substract(double x, double y)
+        {
+            return x - y;
+        }
+
+        public double Multiple(double x, double y)
+        {
+            return x * y;
+        }
+        
+        public double Divide(double x, double y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("Nie można dzielić przez zero");
+                return 0;
+            }
+            else
+                return x / y;
+        }
     }
 }
